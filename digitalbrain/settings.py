db1f37a7210576b4e6b92db83e3cc6c40d94405c
Maiuran Loganathan <maiuran.loganathan@minerva.kgi.edu>
2020-06-13 13:25:18 +0200

    new database

--- digitalbrain/settings.py
+++ digitalbrain/settings.py
@@ -98,34 +98,34 @@ WSGI_APPLICATION = 'digitalbrain.wsgi.application'
 #    }
 #}
 
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+#import dj_database_url
+#db_from_env = dj_database_url.config(conn_max_age=600)
+#DATABASES['default'].update(db_from_env)
 
 
-#if 'RDS_DB_NAME' in os.environ:
-#    DATABASES = {
-#        'default': {
-#            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#            'NAME': os.environ['RDS_DB_NAME'],
-#            'USER': os.environ['RDS_USERNAME'],
-#            'PASSWORD': os.environ['RDS_PASSWORD'],
-#            'HOST': os.environ['RDS_HOSTNAME'],
-#            'PORT': os.environ['RDS_PORT'],
-#        }
-#    }
-#else:
-#    DATABASES = {
-#        'default': {
-            #'ENGINE': 'django.db.backends.sqlite3',
-            #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#            'ENGINE': 'django.db.backends.postgresql',
-#            'NAME': 'digitalbrain',
-#           'USER': 'postgres',
-#            'PASSWORD': '1404',
-#          'HOST': 'LOCALHOST'
-#        }
-#    }
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'digitalbrain',
+           'USER': 'postgres',
+            'PASSWORD': '1404',
+          'HOST': 'LOCALHOST'
+        }
+    }
 
 
 
