b7f3260944ac327fbb72ab970f9dcda68c85d951
Maiuran Loganathan <maiuran.loganathan@minerva.kgi.edu>
2020-06-13 13:12:07 +0200

    new database

--- digitalbrain/settings.py
+++ digitalbrain/settings.py
@@ -28,7 +28,7 @@ DEBUG = False
 ALLOWED_HOSTS = ['myfinalbrain.herokuapp.com']
 
  
-# Application definition
+# Application definitions
 
 INSTALLED_APPS = [
     'brainplatform.apps.BrainplatformConfig',
@@ -87,29 +87,42 @@ WSGI_APPLICATION = 'digitalbrain.wsgi.application'
 #    }
 #}
 
-if 'RDS_DB_NAME' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
-        }
+
+DATABASES = { 
+    'default':{
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.path.join(BASE_DIR, 'db,postgresql')
     }
-else:
-    DATABASES = {
-        'default': {
+}
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
+
+#if 'RDS_DB_NAME' in os.environ:
+#    DATABASES = {
+#        'default': {
+#            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#            'NAME': os.environ['RDS_DB_NAME'],
+#            'USER': os.environ['RDS_USERNAME'],
+#            'PASSWORD': os.environ['RDS_PASSWORD'],
+#            'HOST': os.environ['RDS_HOSTNAME'],
+#            'PORT': os.environ['RDS_PORT'],
+#        }
+#    }
+#else:
+#    DATABASES = {
+#        'default': {
             #'ENGINE': 'django.db.backends.sqlite3',
             #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'digitalbrain',
-            'USER': 'postgres',
-            'PASSWORD': '1404',
-            'HOST': 'LOCALHOST'
-        }
-    }
+#            'ENGINE': 'django.db.backends.postgresql',
+#            'NAME': 'digitalbrain',
+#           'USER': 'postgres',
+#            'PASSWORD': '1404',
+#          'HOST': 'LOCALHOST'
+#        }
+#    }
 
 
 
