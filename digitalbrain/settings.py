a589b0b42076823b41a0573667bc7967e587ee26
Maiuran Loganathan <maiuran.loganathan@minerva.kgi.edu>
2020-06-13 11:25:45 +0200

    with updated static settings

--- digitalbrain/settings.py
+++ digitalbrain/settings.py
@@ -167,5 +167,5 @@ prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
 PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'digitalbrain.static')
 STATIC_URL = '/static/'
